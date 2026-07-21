6baf59836f969ff51bb9ff7071ff6313a3ed684f
mohidreekh <mohammad.idreekh00@gmail.com>
2026-07-21 14:56:58 +0300

    add N-Ary Tree

--- learn trees.cpp
+++ learn trees.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 
 struct Node {
-	int item;
-	Node* left;
-	Node* right;
+    int item;
+    Node* left;
+    Node* right;
 
     int height = 0;
 };
@@ -139,21 +139,21 @@ Node* insert(Node* root, int val) {
 
     int balance = getBalance(root);
 
-    if (balance > 1 && val < root->left->item){
+    if (balance > 1 && val < root->left->item) {
         // L-L
         return rightRotate(root);
     }
-    else if (balance > 1 && val > root->left->item){
+    else if (balance > 1 && val > root->left->item) {
         // L-R
         root->left = leftRotate(root->left);
         return rightRotate(root);
     }
-    else if (balance < -1 && val < root->right->item){
+    else if (balance < -1 && val < root->right->item) {
         // R-L
         root->right = rightRotate(root->right);
         return leftRotate(root);
     }
-    else if (balance < -1 && val > root->right->item){
+    else if (balance < -1 && val > root->right->item) {
         // R-R
         return leftRotate(root);
     }
@@ -174,21 +174,96 @@ void travers(Node* root) {
 
 }
 
+#include<vector>
+#include <memory>
+#include<algorithm>
+#include <queue>
+
+struct NaryNode {
+    int val;
+    
+    vector<NaryNode*> children;
+    
+    NaryNode(int data): val(data){}
+};
+
+
+NaryNode* findNaryNode(NaryNode* root, int val) {
+
+    queue<NaryNode*> openList;
+    openList.push(root);
+
+    while (!openList.empty()) {
+
+        NaryNode* node = openList.front();
+        openList.pop();
+
+        if (node->val == val)
+        {
+            return node;
+        }
+
+        for (int i = 0; i < node->children.size(); i++)
+        {
+            if (node->children[i]->val == val) {
+                return node->children[i];
+            }
+
+            openList.push(node->children[i]);
+      
+        }
+    }
+
+    return nullptr;
+
+}
+
 int main() {
-    //AVL Tree
-	Node* root = new Node;
-    root->item = 10;
-    root->left = nullptr;
-    root->right = nullptr;
+    //N-ary Tree
 
-    int arr[10] = { 20,15,7,3,23,50,33,8,1,5 };
-    int x;
-	for (int i = 0; i < 10; i++)
-	{
-		insert(root, arr[i]);
-	}
+    NaryNode* root = new NaryNode(10);
+    root->children.push_back(new NaryNode(5));
+    root->children.push_back(new NaryNode(4));
+    root->children.push_back(new NaryNode(10));
+    root->children.push_back(new NaryNode(10));
+    root->children.push_back(new NaryNode(10));
+    root->children.push_back(new NaryNode(10));
+    root->children.push_back(new NaryNode(10));
 
-    travers(root);
+    root->children.at(1)->children.push_back(new NaryNode(6));
+    root->children.at(0)->children.push_back(new NaryNode(8));
+    root->children.at(2)->children.push_back(new NaryNode(45));
+    root->children.at(0)->children.push_back(new NaryNode(17));
+
+    cout << root->children.size();
+
+    auto it = remove_if(root->children.begin(),
+        root->children.end(),
+        [](NaryNode* child)
+        {
+            return child->val == 10;
+        });
+
+    root->children.erase(it, root->children.end());
+
+    cout << "\n\n" << root->children.size() << endl;;
+
+    cout << findNaryNode(root, 17)->val << endl;;
+
+    ////AVL Tree
+    //Node* root = new Node;
+    //root->item = 10;
+    //root->left = nullptr;
+    //root->right = nullptr;
+
+    //int arr[10] = { 20,15,7,3,23,50,33,8,1,5 };
+    //int x;
+    //for (int i = 0; i < 10; i++)
+    //{
+    //    insert(root, arr[i]);
+    //}
+
+    //travers(root);
 
 
     //Tnode* root = nullptr;
